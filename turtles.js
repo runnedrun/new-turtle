963e7e6ddc5d31c0d4064d4aa73596ba1bca92cd
David Gaynor <runnedrun@gmail.com>
2016-11-06 14:41:40 -0800

    Classadoo autocommit

--- turtles.js
+++ turtles.js
@@ -1,11 +1,11 @@
 var _heading = 0
 var _position = {x: 0, y: 0};
 var _actionInProgress;
-var _turtle = $(\"#turtle\");
+var _turtle = $("#turtle");
 
-                 function turnRight() {
-                _heading = _heading + 90  
-                animateRotate(_heading, _turtle);
+function turnRight() {
+  _heading = _heading + 90  
+  animateRotate(_heading, _turtle);
 }
 
 function turnLeft() {
@@ -35,33 +35,19 @@ function goForward(distance) {
 
 }
 
-function insertLine(origin, angle, magnitude) {
-  var line = $(\"<div class='line'></div>\");
-                console.log(\"rotate\", \"rotate(\" + angle + \"deg)\") 
-                line.css({
-                left: origin.x + _turtle.width() / 2,    
-               bottom: origin.y + _turtle.height() / 2,
-      width: magnitude,
-        transform: \"rotate(\" + angle + \"deg)\",
-        \"transform-origin\": \"top left\"
-})
 
-$(\"#turtle-cage\").append(line);
-   console.log($(\"#turtle-cage\"))
-   }
-
-   function animateRotate(angle, element) {    
+function animateRotate(angle, element) {    
   // we use a pseudo object for the animation
   // (starts from `0` to `angle`), you can name it as you want
   $({deg: 0}).animate({deg: angle}, {
-  duration: 100,
-  step: function(now) {
-    // in the step-callback (that is fired each step of the animation),
-    // you can use the `now` paramter which contains the current
-    // animation-position (`0` up to `angle`)
-    element.css({
-      transform: 'rotate(' + now + 'deg)'
-    });
-  }
-});
+    duration: 100,
+    step: function(now) {
+      // in the step-callback (that is fired each step of the animation),
+      // you can use the `now` paramter which contains the current
+      // animation-position (`0` up to `angle`)
+      element.css({
+        transform: 'rotate(' + now + 'deg)'
+      });
+    }
+  });
 }
